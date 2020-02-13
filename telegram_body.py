8a9cc8252a7232f10484f0bf1927a08ee675e0c0
Kapral ALeksandr <a.kapral26@gmail.com>
2020-02-13 19:30:16 +0000

    tabs/\s

--- telegram_body.py
+++ telegram_body.py
@@ -21,12 +21,12 @@ class Telegram_bot:
 
         @bot.message_handler(commands=['start', 'help'])
         def send_welcome(message):
-	        bot.reply_to(message, "Howdy, how are you doing?")
+            bot.reply_to(message, "Howdy, how are you doing?")
 
         @bot.message_handler(func=lambda message: True)
         def echo_all(message):
             [x for x in dir(message)]
-	        bot.reply_to(message, message.text)
+            bot.reply_to(message, message.text)
 
         bot.polling()
 
