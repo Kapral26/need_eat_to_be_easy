f5a6e6222f7dd616865668ba3a1ef042311b3eb8
Kapral ALeksandr <a.kapral26@gmail.com>
2020-02-13 22:38:39 +0300

    new version

---
+++ telegram_body.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from os import path
+import telebot
+
+
+
+class Telegram_bot:
+    def __init__(self):
+        self.get_bot_key()
+        self.for_bot_test()
+
+    def get_bot_key(self):
+        key_file = path.join(path.dirname(__file__), "telegram_key.txt")
+        with open(key_file) as file:
+            self.token = file.read()
+        print(self.token)
+
+    def for_bot_test(self):
+
+        bot = telebot.TeleBot(self.token)
+
+        @bot.message_handler(commands=['start', 'help'])
+        def send_welcome(message):
+	        bot.reply_to(message, "Howdy, how are you doing?")
+
+        @bot.message_handler(func=lambda message: True)
+        def echo_all(message):
+            [print(x) for x in dir(message)]
+	        bot.reply_to(message, message.text)
+
+        bot.polling()
+
+if __name__ == "__main__":
+    Telegram_bot()
